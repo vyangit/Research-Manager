5e5fc6799bd3ae14184fbb640378a0918f2e934d
Victor Yan <vyan.git@gmail.com>
2019-05-15 18:44:47 -0700

    1) Set up session data querying

--- js/popupHandler.js
+++ js/popupHandler.js
@@ -1,27 +1,62 @@
+"use strict"
+
 $(window).on('load', initialize);
 
 function initialize() {
-    $('#on-off-switch')[0].addEventListener('click', toggleOnOff);
+    linkEventListeners();
     populateSessionDropdown();
 }
 
-function populateSessionDropdown() {
-    var sessions = window.localStorage.getItem("sessions");
-
-    var option = $('<option/>', {value:'hi', text:'hi'});
-    $('#session-selector').append(option);
-} 
-
-function toggleOnOff(e) {
-    if ($('#on-off-switch-label').text() == 'On') {
-        $('#on-off-switch-label').text('Off');
-        $('#popup-main-interface').attr('hidden', true);
-    } else {
-        $('#on-off-switch-label').text('On');
-        $('#popup-main-interface').show();
-        $('#popup-main-interface').removeAttr('hidden');
-    }
+function linkEventListeners() {
+    $('#on-off-switch')[0].addEventListener('click', toggleOnOffListener);
 }
+
+function toggleOnOffListener(e) {
+    browser.storage.local.set({'isOn':e.target.checked})
+    setOnOff();
+}
+
+function setOnOff() {
+    browser.storage.local.get('isOn').then((res) => {
+        if (res.isOn) {
+            $('#on-off-switch-label').text('On');
+            $('#on-off-switch').attr('checked', true);
+            $('#popup-main-interface').attr('hidden', false);
+        } else {
+            $('#on-off-switch-label').text('Off');
+            $('#on-off-switch').attr('checked', false);
+            $('#popup-main-interface').attr('hidden', true);
+        }
+    });
+}
+
+function populateSessionDropdown() {
+    browser.storage.local.get('sessions', (res) => {
+        if (typeof res.sessions === 'undefined' || res.sessions.length == 0) {
+            let option = $('<option/>', {
+                value:'Create a session...', 
+                text:'Create a session...',
+                disabled: true,
+                selected: true,
+                hidden: true,
+            });
+            $('#session-selector').append(option);
+        } else {
+            let placeholder = $('<option/>', {
+                value:'Select a session...', 
+                text:'Select a session...',
+                disabled: true,
+                selected: true,
+                hidden: true,
+            });
+            $('#session-selector').append(option);
+            for (let session in res.sessions) {
+                let option = $('<option/>', {value:'session.label', text:'session.label'});
+                $('#session-selector').append(option);
+            }
+        }
+    });
+} 
 
 function switchSession(sessionTitle) {
 
@@ -31,12 +66,16 @@ function switchTab(tabTitle) {
     clearTabComponents();
     switch(tabTitle) {
         case 'Tabs': 
+            populateTabsView();
             break;
         case 'Quotes':
+            populateQuotesView() 
             break;
         case 'Citations':
+            populateCitationsView()
             break;
         case 'Datasets':
+            populateDatasetsView()
             break;
     }
 }
